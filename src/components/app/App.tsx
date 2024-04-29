417601a46e51cbd7f3a1d4af648aa9daef220ecf
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-30 00:57:12 +0200

    Create useStore

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,69 +1,24 @@
+import { useStore } from '../../hooks/useStore'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import { useReducer } from 'react'
-import { State, Action } from '../../types'
-
-const initialState: State = {
-  fromLanguage: 'auto',
-  toLanguage: 'en',
-  fromText: '',
-  result: '',
-  loading: false,
-}
-
-function reducer(state: State, action: Action) {
-  const { type } = action
-
-  if (type === 'INTERCHANGE_LANGUAGES') {
-    return {
-      ...state,
-      fromLanguage: state.toLanguage,
-      toLanguage: state.fromLanguage,
-    }
-  }
-
-  if (type === 'SET_FROM_LANGUAGE') {
-    return {
-      ...state,
-      fromLanguage: action.payload,
-    }
-  }
-
-  if (type === 'SET_TO_LANGUAGE') {
-    return {
-      ...state,
-      toLanguage: action.payload,
-    }
-  }
-
-  if (type === 'SET_FROM_TEXT') {
-    return {
-      ...state,
-      loading: true,
-      fromText: action.payload,
-      result: '',
-    }
-  }
-
-  if (type === 'SET_RESULT') {
-    return {
-      ...state,
-      loading: true,
-      result: action.payload,
-    }
-  }
-
-  return state
-}
 
 function App() {
-  const [] = useReducer(reducer, initialState)
+  const { fromLanguage, setFromLanguage } = useStore()
+
   return (
-    <>
-      <div className='app'>
-        <h1>Clon - Google Translate</h1>
-      </div>
-    </>
+    <div className='app'>
+      <h1>Google Translate</h1>
+
+      <button
+        onClick={() => {
+          setFromLanguage('es')
+        }}
+      >
+        Cambiar a Español
+      </button>
+
+      {fromLanguage}
+    </div>
   )
 }
 
