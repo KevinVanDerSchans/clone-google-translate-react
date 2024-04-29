e93759e01e59d86811cfcee377eca7ee50c7eb72
Kevin Schans <kevinvdsd@hotmail.com>
2024-04-29 20:27:11 +0200

    Start reducer

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,62 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
+
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false,
+}
+
+function reducer(state, action) {
+  const { type, payload } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage,
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: payload,
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: payload,
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: payload,
+      result: '',
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: true,
+      result: payload,
+    }
+  }
+
+  return state
+}
 
 function App() {
+  const [] = useReducer(reducer, initialState)
   return (
     <>
       <div className='app'>
